d906d642ad1894c3e8ac928ff86a5b2b88fe6174
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-18 21:41:06 +0000

    initial commit

---
+++ 0x0B-web_server/fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+from fabric.api import run, hosts, local, put, env
+
+
+env.user = "ubuntu"
+env.password = ""
+env.key_filename = "~/.ssh/holberton"
+
+
+def pack:
+    local("tar --exclude='*.tar.gz' -cvzf holbertonwebapp.tar.gz .")
+
+
+def deploy:
+    put("holbertonwebapp.tar.gz", "/tmp")
+    run("mkdir /tmp/holbertonwebapp")
+    run("tar -xvf /tmp/holbertonwebapp.tar.gz -C /tmp/holbertonwebapp")
+
+
+def clean:
+    local("rm holbertonwebapp.tar.gz")
