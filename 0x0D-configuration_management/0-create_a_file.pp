e0aa157548168f3ff1c7906f277b2b53b7305ef1
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-26 19:06:04 +0000

    initial commit:

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#creating a holberton file in /tmp/ directory
+file {'/tmp/holberton':
+  path    => '/tmp/',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
