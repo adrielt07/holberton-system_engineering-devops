ec9a6a3e83b88024476cbbd6c0c072d9024cbe0a
adrielt07 <adrieltolentino@outlook.com>
2018-08-30 21:14:43 +0000

    added documentation for method

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    '''
+    Retrieves number of subscribers to a subreddit
+    - If the subreddit does not exists, return 0
+    Usage:
+       number_of_subscribers(programming)
+       Returns the number of subscribers the 'programming' subreddit has
+    '''
     url = "https://api.reddit.com/r/{}/about".format(subreddit)
     headers = {'user-agent': 'my-app/0.0.1'}
     status = requests.get(url, headers=headers)
